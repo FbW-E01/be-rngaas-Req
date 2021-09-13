c0c6fa768a12cc5ae8dbbc3658c4ffa108079f06
Joel Peltonen <joel.peltonen@digitalcareerinstitute.org>
2021-09-13 11:21:25 +0200

    Add env example

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
+import dotenv from 'dotenv'
 import http from 'http';
+
+// Reading values from the .env file into process.env
+dotenv.config();
+
+// Printing out an example of a value stored in .env
+console.log("API_KEY is " + process.env.API_KEY)
 
 function requestHandler(request, response) {
     console.log("Request received, making random number!")
-    const randomNumber = Math.random() * 500000;
-    response.end(randomNumber.toString());
+    const randomNumber = Math.round(Math.random() * 99999);
+    response.end(process.env.API_KEY + "___" + randomNumber.toString());
 }
 
 const myServer = http.createServer(requestHandler);
 
-console.log("Server is starting");
+console.log("Server is starting http://localhost:" + process.env.PORT);
 
-myServer.listen(8081);
+myServer.listen(parseInt(process.env.PORT));
